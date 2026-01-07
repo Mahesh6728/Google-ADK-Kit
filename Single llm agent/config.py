1ed5a13ac0532f886bce92ab3a5fa6128e42ffa4
Mahesh Reddy <lovelymahesh6728@gmail.com>
2026-01-08 01:40:41 +0530

    Add .env support for API keys

--- Single llm agent/config.py
+++ Single llm agent/config.py
@@ -1,6 +1,12 @@
 from google import genai
 import os
-#genai.configure(api_key=os.getenv("AIzaSyAxKwaRA9FtK-7j-DkEql0CL41tb44hrPs"))  #
-#client = genai.Client(api_key= "AIzaSyAxKwaRA9FtK-7j-DkEql0CL41tb44hrPs") # Set your API key here or use environment variable
+from dotenv import load_dotenv
+load_dotenv() 
+api_key = os.getenv("Mahi_API_KEY")
+#client = genai.Client(api_key=api_key)
+#genai.configure(api_key=api_key)
+#client = genai.Client(api_key=api_key") # Set your API key here or use environment variable
 MODEL = "gemini-3-flash-preview" # Specify the model to use
-client = genai.Client(api_key ="AIzaSyAxKwaRA9FtK-7j-DkEql0CL41tb44hrPs")
+client = genai.Client(api_key = api_key) # Create a client instance
+
+print(api_key)
